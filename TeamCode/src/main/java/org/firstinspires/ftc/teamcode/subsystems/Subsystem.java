cafa40884cfe53b2ffd3c0c75097d97634db423f
sid <siddharth.rajram@gmail.com>
2026-06-30 16:54:47 -0400

    Added tag for Logging to Subsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystem.java
@@ -6,15 +6,20 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.utils.RobotState;
 
 public abstract class Subsystem {
+    protected String tag;
     protected HardwareMap hardwareMap;
     protected Telemetry telemetry;
     protected RobotState robotState;
+
+    public Subsystem(String tag){
+        this.tag = tag;
+    }
     public abstract void initLoop();
     public abstract void justAfterStart();
     public abstract void periodic();
     public abstract void close();
 
-    public void getParameters(HardwareMap hardwareMap, Telemetry telemetry, RobotState robotState){
+    public void setParameters(HardwareMap hardwareMap, Telemetry telemetry, RobotState robotState){
         this.hardwareMap = hardwareMap;
         this.telemetry = telemetry;
         this.robotState = robotState;
