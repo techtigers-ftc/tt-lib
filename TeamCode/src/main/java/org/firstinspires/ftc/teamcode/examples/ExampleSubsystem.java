978ae1ba6061b8402149063e6d89aa3f20bcd5a4
sid <siddharth.rajram@gmail.com>
2026-06-30 16:55:08 -0400

    Added Logging and Telemetry to ExampleSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleSubsystem.java
@@ -2,14 +2,17 @@ package org.firstinspires.ftc.teamcode.examples;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.teamcode.subsystems.Subsystem;
+import org.firstinspires.ftc.teamcode.utils.TTLogger;
 
 public class ExampleSubsystem extends Subsystem {
     private DcMotor exampleMotor;
     private Servo exampleServo;
 
     public ExampleSubsystem() {
+        super("ExampleSubsystem");
         hardwareMap.get(DcMotor.class, "example_motor");
         hardwareMap.get(Servo.class, "example_servo");
     }
@@ -26,6 +29,8 @@ public class ExampleSubsystem extends Subsystem {
     @Override
     public void periodic() {
         exampleMotor.setPower(0.5);
+        telemetry.addData("Example Motor Power", exampleMotor.getPower());
+        TTLogger.dd(tag, "Example Motor Power: %.2f", exampleMotor.getPower());
     }
 
     @Override
