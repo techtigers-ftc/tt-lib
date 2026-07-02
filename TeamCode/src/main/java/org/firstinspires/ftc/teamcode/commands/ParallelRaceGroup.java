0d77d0d48b8db703a0bf5bafa40de5fa759c6989
sid <siddharth.rajram@gmail.com>
2026-07-02 09:27:33 -0400

    Added ParallelRaceGroup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ParallelRaceGroup.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+public class ParallelRaceGroup extends CommandGroup {
+
+    public ParallelRaceGroup(Command... commands) {
+        super();
+        addCommands(commands);
+    }
+
+    @Override
+    public void initialize() {
+        for (Command command : commands) {
+            command.initialize();
+        }
+    }
+
+    @Override
+    public void update() {
+        for (Command command : commands) {
+            if (command.isFinished()){
+                command.end(false);
+                commands.remove(command);
+                commands.forEach(c -> c.end(true));
+            } else {
+                command.update();
+            }
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        for (Command command : commands) {
+            if (command.isFinished()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
