61892909a37004d45540594095c2fba3e68cd496
sid <siddharth.rajram@gmail.com>
2026-07-07 19:45:36 -0400

    Debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/SequentialCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/SequentialCommandGroup.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.commands;
 
 
+import org.firstinspires.ftc.teamcode.utils.TTLogger;
+
 public class SequentialCommandGroup extends CommandGroup {
     private Command currentCommand;
 
@@ -10,6 +12,7 @@ public class SequentialCommandGroup extends CommandGroup {
 
     @Override
     public void initialize() {
+        TTLogger.dd(tag, "Initalize Running on Sequential Command Greoup");
         currentCommand = commands.get(0);
         currentCommand.initialize();
     }
@@ -19,13 +22,21 @@ public class SequentialCommandGroup extends CommandGroup {
         currentCommand.update();
         if (currentCommand.isFinished()) {
             currentCommand.end(false);
-            currentCommand = commands.get(commands.indexOf(currentCommand) + 1);
-            currentCommand.initialize();
+            try {
+                currentCommand = commands.get(commands.indexOf(currentCommand) + 1);
+                currentCommand.initialize();
+            } catch (IndexOutOfBoundsException e) {
+                // No more commands to execute
+            }
         }
     }
 
     @Override
     public boolean isFinished() {
+        TTLogger.dd(tag, "Is Finished: %b", commands.get(commands.size() - 1).isFinished());
+        for (Command command: commands){
+            TTLogger.dd(tag, "Each Command Is Finished %b", command.isFinished());
+        }
         return commands.get(commands.size() - 1).isFinished();
     }
 }
