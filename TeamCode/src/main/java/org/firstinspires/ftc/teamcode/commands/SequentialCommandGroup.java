771c071032047729c31fd9f8a654347b6ad1fcd3
sid <siddharth.rajram@gmail.com>
2026-07-09 18:33:23 -0400

    Somehow works?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/SequentialCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/SequentialCommandGroup.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.commands;
 
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.teamcode.utils.TTLogger;
 
 public class SequentialCommandGroup extends CommandGroup {
     private Command currentCommand;
+    private ElapsedTime timer;
+    private boolean isFinished;
 
     public SequentialCommandGroup(Command... commands) {
         addCommands(commands);
+        timer = new ElapsedTime();
     }
 
     @Override
@@ -15,15 +20,20 @@ public class SequentialCommandGroup extends CommandGroup {
 //        TTLogger.dd(tag, "Initialize Running on Sequential Command Group");
         currentCommand = commands.get(0);
         currentCommand.initialize();
+        timer.reset();
     }
 
     @Override
     public void update() {
         if (currentCommand.isFinished()) {
             currentCommand.end(false);
+            TTLogger.dd(tag, "End Ran on Previous Command");
             if ((commands.indexOf(currentCommand) + 1) < commands.size()) {
                 currentCommand = commands.get(commands.indexOf(currentCommand) + 1);
                 currentCommand.initialize();
+                TTLogger.dd(tag, "Init Ran on Next Command");
+            } else {
+                isFinished = true;
             }
         } else {
             currentCommand.update();
@@ -32,19 +42,11 @@ public class SequentialCommandGroup extends CommandGroup {
 
     @Override
     public boolean isFinished() {
-        for (Command command : commands) {
-            if (!command.isFinished()) {
-                return false;
-            }
-        }
-
-        return true;
+        return isFinished;
     }
 
     @Override
     public void end(boolean interrupted){
-       for (Command command: commands){
-           command.initialize();
-       }
+        isFinished = false;
     }
 }
