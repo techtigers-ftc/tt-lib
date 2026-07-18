424550f93290679c3d1ed1742b7022926f6142fb
sid <siddharth.rajram@gmail.com>
2026-07-18 19:00:24 -0400

    Added GamepadEx along with GamepadButtons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/GamepadButtons.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.gamepad;
+
+public enum GamepadButtons {
+    A,
+    B,
+    X,
+    Y,
+    LEFT_BUMPER,
+    RIGHT_BUMPER,
+    LEFT_STICK_BUTTON,
+    RIGHT_STICK_BUTTON,
+    DPAD_UP,
+    DPAD_DOWN,
+    DPAD_LEFT,
+    DPAD_RIGHT,
+    START,
+    BACK,
+    RIGHT_TRIGGER,
+    LEFT_TRIGGER
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/GamepadEx.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.gamepad;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class GamepadEx {
+    private Gamepad gamepad;
+    public GamepadEx(Gamepad gamepad) {
+        this.gamepad = gamepad;
+    }
+
+    public Trigger getGamepadButton(GamepadButtons button) {
+        switch(button) {
+            case A:
+                return new Trigger(() -> gamepad.a);
+            case B:
+                return new Trigger(() -> gamepad.b);
+            case X:
+                return new Trigger(() -> gamepad.x);
+            case Y:
+                return new Trigger(() -> gamepad.y);
+            case LEFT_BUMPER:
+                return new Trigger(() -> gamepad.left_bumper);
+            case RIGHT_BUMPER:
+                return new Trigger(() -> gamepad.right_bumper);
+            case LEFT_STICK_BUTTON:
+                return new Trigger(() -> gamepad.left_stick_button);
+            case RIGHT_STICK_BUTTON:
+                return new Trigger(() -> gamepad.right_stick_button);
+            case DPAD_UP:
+                return new Trigger(() -> gamepad.dpad_up);
+            case DPAD_DOWN:
+                return new Trigger(() -> gamepad.dpad_down);
+            case DPAD_LEFT:
+                return new Trigger(() -> gamepad.dpad_left);
+            case DPAD_RIGHT:
+                return new Trigger(() -> gamepad.dpad_right);
+            case START:
+                return new Trigger(() -> gamepad.start);
+            case  BACK:
+                return new Trigger(() -> gamepad.back);
+            default:
+                return new Trigger(() -> false);
+        }
+    }
+
+    public void rumble(Gamepad.RumbleEffect rumbleEffect) {
+        gamepad.runRumbleEffect(rumbleEffect);
+    }
+
+    public void rumble(double leftMotorPower, double rightMotorPower, int duration) {
+        gamepad.rumble(leftMotorPower, rightMotorPower, duration);
+    }
+}
