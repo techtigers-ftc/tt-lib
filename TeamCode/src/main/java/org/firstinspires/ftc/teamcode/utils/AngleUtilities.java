85a867e0578eec803c7c6a045ad6f0bd00f2c8d2
Josh Raikman <136274476+JoshR654@users.noreply.github.com>
2026-07-22 10:47:33 -0400

    added angle utilities & robot save state

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AngleUtilities.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+/**
+ * Various utilities for working with angles.
+ */
+public class AngleUtilities {
+    public static final double TAU = Math.PI * 2;
+
+    /**
+     * Returns [angle] clamped to `[0, TAU]`.
+     *
+     * @param angle angle measure in radians
+     */
+    public static double norm(double angle) {
+        double modifiedAngle = angle % TAU;
+
+        modifiedAngle = (modifiedAngle + TAU) % TAU;
+
+        return modifiedAngle;
+    }
+
+    /**
+     * Returns distance from target position - current position
+     *
+     * @param currentPosition the current position
+     * @param targetPosition the target position
+     * @return angle error clamped to '[-PI, PI]'
+     */
+    public static double normDelta(double currentPosition, double targetPosition) {
+        double modifiedAngleDelta = norm(targetPosition - currentPosition);
+
+        if (modifiedAngleDelta > Math.PI) {
+            modifiedAngleDelta -= TAU;
+        }
+
+        return modifiedAngleDelta;
+    }
+
+}
