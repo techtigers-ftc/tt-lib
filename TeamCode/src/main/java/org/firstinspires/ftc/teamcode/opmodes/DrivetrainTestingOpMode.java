1da1f8b64f19b07200a0d78cf25035959cbcc365
Josh Raikman <136274476+JoshR654@users.noreply.github.com>
2026-07-22 10:20:49 -0400

    manual override to start a timer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DrivetrainTestingOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DrivetrainTestingOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.bylazar.telemetry.JoinedTelemetry;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.GoBodometrySubsystem;
@@ -15,7 +16,10 @@ public class DrivetrainTestingOpMode extends BaseOpMode {
     private double maxCurrentDraw = 0;
     private double totalCurrentDraw = 0;
     private double averageCurrentDraw = 0;
-    private int currentDrawCount = 0;
+    private boolean manualOverride = false;
+    private double elapsed = 0;
+    private ElapsedTime timer = new ElapsedTime();
+    private int count = 0;
     private DriveSubsystem drive;
     JoinedTelemetry joinedTelemetry = new JoinedTelemetry(PanelsTelemetry.INSTANCE.getFtcTelemetry(), telemetry);;
 
@@ -29,19 +33,22 @@ public class DrivetrainTestingOpMode extends BaseOpMode {
 
     @Override
     protected void update() {
-        boolean driving = gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x ==  0;
+        boolean driving = gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x !=  0;
         drive.driveRobotCentric(gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
+        manualOverride = gamepad1.dpadLeftWasPressed() != manualOverride;
 
         double velocity = robotState.getRobotVelocity().getPoint().magnitude();
         double currentDraw = robotState.getDriveCurrent();
 
-        if (!driving) {
+        if (!driving && !manualOverride) {
             // reset count and total if you stop driving
             totalCurrentDraw = 0;
-            currentDrawCount = 0;
+            count = 0;
+            timer.reset();
         } else {
             totalCurrentDraw += currentDraw;
-            averageCurrentDraw = totalCurrentDraw / ++currentDrawCount;
+            averageCurrentDraw = totalCurrentDraw / ++count;
+            elapsed = timer.seconds();
         }
 
         if (velocity > maxVelocity) {
@@ -61,11 +68,15 @@ public class DrivetrainTestingOpMode extends BaseOpMode {
             drive.setMotorPowers(0, 0, 0, 1);
         }
 
+        joinedTelemetry.addData("Manual override", manualOverride);
+        joinedTelemetry.addLine();
+        joinedTelemetry.addData("Time Elapsed", elapsed);
+        joinedTelemetry.addLine();
         joinedTelemetry.addData("Current Velocity", velocity);
-        joinedTelemetry.addData("Current Draw", currentDraw);
-        joinedTelemetry.addData("Average current draw", averageCurrentDraw);
-
         joinedTelemetry.addData("Max Velocity", maxVelocity);
+        joinedTelemetry.addLine();
+        joinedTelemetry.addData("Current Draw", currentDraw);
+        joinedTelemetry.addData("Average Current Draw", averageCurrentDraw);
         joinedTelemetry.addData("Max Current Draw", maxCurrentDraw);
         joinedTelemetry.update();
 
