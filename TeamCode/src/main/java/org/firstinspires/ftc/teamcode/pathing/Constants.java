ecd89dc170fed490364e0cc1bc4393b5b39538d4
Thornblaster12345 <alexvinuk@icloud.com>
2026-07-30 19:45:13 -0400

    added constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathing/Constants.java
@@ -5,6 +5,7 @@ import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.subsystems.AutoSubsystem;
@@ -13,12 +14,21 @@ import org.firstinspires.ftc.teamcode.utils.RobotState;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .centripetalScaling(0);
+            .centripetalScaling(0)
+            .mass(5.443);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1);
+            .maxPower(1)
+            .rightFrontMotorName("right_front")
+            .rightRearMotorName("right_rear")
+            .leftRearMotorName("left_rear")
+            .leftFrontMotorName("left_front")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap, DriveSubsystem drive, RobotState robotState) {
         return new Follower(followerConstants, new RobotStateLocalizer(robotState), new Mecanum(hardwareMap, driveConstants, drive, robotState));
