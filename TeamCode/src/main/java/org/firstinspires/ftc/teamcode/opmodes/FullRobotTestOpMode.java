6ab31d9e32af686371580b05297877c11ae78335
SmartSilver5294 <chandra.ayush2021@gmail.com>
2026-08-21 12:23:13 -0400

    Added intake stuff to full robot testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FullRobotTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FullRobotTestOpMode.java
@@ -4,9 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.commands.ManualDriveCommand;
 import org.firstinspires.ftc.teamcode.commands.dropper.UnsafeDropperSlidesCommand;
+import org.firstinspires.ftc.teamcode.commands.intake.ManualIntakeCommand;
+import org.firstinspires.ftc.teamcode.gamepad.GamepadEx;
+import org.firstinspires.ftc.teamcode.gamepad.GamepadKeys;
 import org.firstinspires.ftc.teamcode.gamepad.Trigger;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.DropperSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.utils.TTLogger;
 
 @TeleOp
 public class FullRobotTestOpMode extends BaseOpMode {
@@ -16,16 +21,23 @@ public class FullRobotTestOpMode extends BaseOpMode {
     protected void initialize() {
         DriveSubsystem drive = new DriveSubsystem(hardwareMap);
         dropper = new DropperSubsystem(hardwareMap);
+        IntakeSubsystem intake = new IntakeSubsystem(hardwareMap);
 
-        registerSubsystems(drive, dropper);
+        registerSubsystems(drive, dropper, intake);
 
         Trigger runSlides = new Trigger(() -> manipulatorGamepad.getLeftY() != 0);
         UnsafeDropperSlidesCommand unsafeDropperSlidesCommand = new UnsafeDropperSlidesCommand(dropper, manipulatorGamepad);
         runSlides.whileActiveOnce(unsafeDropperSlidesCommand);
 
+        Trigger manualIntake = new Trigger(() -> manipulatorGamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) != 0 || manipulatorGamepad.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) != 0);
+        ManualIntakeCommand manualIntakeCommand = new ManualIntakeCommand(manipulatorGamepad, intake);
+        manualIntake.whenActive(manualIntakeCommand);
+
         Trigger runDrive = new Trigger(() -> driverGamepad.getLeftY() != 0 || driverGamepad.getLeftX() != 0 || driverGamepad.getRightX() != 0);
         ManualDriveCommand manualDriveCommand = new ManualDriveCommand(drive, robotState, driverGamepad);
         runDrive.whileActiveOnce(manualDriveCommand);
+
+        TTLogger.setLoggingLevel(TTLogger.DISABLED);
     }
 
     @Override
