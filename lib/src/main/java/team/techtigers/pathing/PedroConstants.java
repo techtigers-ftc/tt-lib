05b7ad175803fbe0aef6a7bbc9d0abd1e9158d3a
Josh Raikman <136274476+JoshR654@users.noreply.github.com>
2026-09-10 20:21:18 -0400

    pedro 2.0 consants configuration good

---
+++ lib/src/main/java/team/techtigers/pathing/PedroConstants.java
@@ -0,0 +1,14 @@
+package team.techtigers.pathing;
+
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PredictiveBrakingCoefficients;
+
+public class PedroConstants {
+    public double centripetalScaling;
+    public double mass;
+    public PIDFCoefficients headingPIDFCoefficients;
+    public PredictiveBrakingCoefficients predictiveBrakingCoefficients;
+    public double maxPower;
+    public double xVelocity;
+    public double yVelocity;
+}
