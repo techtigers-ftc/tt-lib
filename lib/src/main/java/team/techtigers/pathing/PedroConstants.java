5f6e2abac97127d2037397bc04ad001ce18ece8c
Josh Raikman <136274476+JoshR654@users.noreply.github.com>
2026-09-10 20:38:41 -0400

    gradle changes🐈

--- lib/src/main/java/team/techtigers/pathing/PedroConstants.java
+++ lib/src/main/java/team/techtigers/pathing/PedroConstants.java
@@ -2,13 +2,15 @@ package team.techtigers.pathing;
 
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.control.PredictiveBrakingCoefficients;
+import com.pedropathing.paths.PathConstraints;
 
 public class PedroConstants {
-    public double centripetalScaling;
-    public double mass;
-    public PIDFCoefficients headingPIDFCoefficients;
-    public PredictiveBrakingCoefficients predictiveBrakingCoefficients;
-    public double maxPower;
-    public double xVelocity;
-    public double yVelocity;
+    // Default values set
+    public double centripetalScaling = 0.0;
+    public double mass = 13.0;
+    public PIDFCoefficients headingPIDFCoefficients = new PIDFCoefficients(2, 0, 0, 0);
+    public PredictiveBrakingCoefficients predictiveBrakingCoefficients = new PredictiveBrakingCoefficients(0.1, 0.047, 0.0017);
+    public double maxPower = 1;
+    public double xVelocity = 45.35;
+    public double yVelocity = 65.18;
 }
