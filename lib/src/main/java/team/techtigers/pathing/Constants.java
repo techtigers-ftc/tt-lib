4fd047a83c290dfe9533968986b58cf92a1cf9f6
Josh Raikman <136274476+JoshR654@users.noreply.github.com>
2026-09-13 12:29:59 -0400

    updated constants + mecanum classes for pedro 3.0

--- lib/src/main/java/team/techtigers/pathing/Constants.java
+++ lib/src/main/java/team/techtigers/pathing/Constants.java
@@ -1,16 +1,25 @@
 package team.techtigers.pathing;
 
+import com.pedropathing.algorithm.Foresight;
+import com.pedropathing.algorithm.ForesightConfig;
 import com.pedropathing.follower.Follower;
 
 import team.techtigers.subsystems.DriveSubsystem;
 import team.techtigers.utils.RobotState;
 
+/**
+ *
+ */
 public class Constants {
-
-
-    public static Follower createFollower(DriveSubsystem drive, RobotState robotState, PedroConfig pedroConfig) {
-
-
-        return new Follower(new RobotStateLocalizer(robotState), new Mecanum(driveConstants, drive, robotState));
+    /**
+     * Constructs a new pedro pathing follower object
+     *
+     * @param drive the drive subsystem
+     * @param robotState the robot state
+     * @param foresightConfig the foresight config, defined from the auto tuners in the pedro pathing quickstart
+     * @return the follower object created with the given parameters
+     */
+    public static Follower createFollower(DriveSubsystem drive, RobotState robotState, ForesightConfig foresightConfig) {
+        return new Follower(new RobotStateLocalizer(robotState), new Mecanum(drive), new Foresight(foresightConfig));
     }
 }
